24c18e91b03b7ee52830c2871bd38db99058e0b6
RelangiNagasiva <r.nagasiva.com>
2022-02-01 13:57:43 +0530

    Add program for swap of two numbers

---
+++ src/Snumber.java
@@ -0,0 +1,20 @@
+
+public class Snumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Declare the variable for a and b
+		int a=20,b=10;
+		//Adding a and b assign to value to a
+		a = a + b;
+		//subtract a and b assign value to b;
+        b = a - b;
+      //subtract a and b assign value to a;
+        a = a - b;
+        System.out.println("After swapping:"
+                           + " a = " + a + ", b = " + b);
+
+
+	}
+
+}
---
+++ src/swapnumbers.java
@@ -0,0 +1,17 @@
+
+public class swapnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10, b=20,c = 0;
+		//Assign value of a to c;
+		c=a;
+		//Assign value of a to b;
+		a=b;
+		//Assign value of b to c;
+		b=c;	
+		 System.out.println("After swapping:"
+                 + " a = " + a + ", b = " + b);
+	}
+
+}
